b5d8a79d699c18aedd75c51772b21f9c47ceefb4
Pranav Akbari <pranavakbari07@gmail.com>
2025-12-18 09:26:22 +0530

    intro

--- Make Server using http/index.js
+++ Make Server using http/index.js
@@ -1,16 +1,3 @@
-// const http = require("http")
-// const port = 1008
-
-// const requestHandler = (req,res)=>{
-//     res.write("Hello This is Node Server")
-//     res.end()
-// }
-
-// const server = http.createServer(requestHandler)
-
-// server.listen(port,(err)=>{
-//     err ? console.log(err) : console.log(`Server is running on port ${port}`,)
-// })
 
 
 const http = require("http")
