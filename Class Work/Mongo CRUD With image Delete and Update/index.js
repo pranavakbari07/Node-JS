d71cc689659dbb637b68257cc849cf6089f87f6a
Pranav Akbari <pranavakbari07@gmail.com>
2026-01-02 11:45:24 +0530

    project

--- Class Work/Mongo CRUD With image Delete and Update/index.js
+++ Class Work/Mongo CRUD With image Delete and Update/index.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const port = 2438
-const path = require("path");
 
 const app = express();
 
 const db = require("./config/db");
 const schema = require("./model/firstSchema");
 const multer = require("./middlewares/multer");
+const path = require("path");
+const fs = require("fs");
 
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
@@ -23,6 +24,8 @@ app.post("/addData",multer,async (req, res) => {
     })
 })
 app.get("/deleteData", async (req, res) => {
+    let singleData = await schema.findById(req.query.id)
+    fs.unlinkSync(singleData.image);
     await schema.findByIdAndDelete(req.query.id).then(() => {
         res.redirect("/");
     })
@@ -31,7 +34,15 @@ app.get("/editData", async(req,res)=>{
     let singleData = await schema.findById(req.query.id)
     res.render("edit",{singleData});
 })
-app.post("/updateData", async(req,res)=>{
+app.post("/updateData", multer, async(req,res)=>{
+    let singleData = await schema.findById(req.body.id)
+    let img = "";
+
+    req.file ? img = req.file.path : img = singleData.image;
+    req.file && fs.unlinkSync(singleData.image);
+
+    req.body.image = img
+
     await schema.findByIdAndUpdate(req.body.id,req.body).then(()=>{
         res.redirect("/");
     })
